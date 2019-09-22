dc48e5a64eb33fe7bd1257be9a361ae65c9ee46c
Waqas Ajaz <waqasajazch@gmail.com>
2019-09-22 14:26:48 +0500

    removed Click event for expandable row icon.

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -158,7 +158,7 @@ class TableBody extends React.Component {
         console.warn(
           'Deprecated: Clicks on expandable button will not trigger onRowClick in an upcoming release, see: https://github.com/gregnb/mui-datatables/issues/516.',
         );
-        this.props.options.onRowClick(row, data, event);
+        // this.props.options.onRowClick(row, data, event);
       }
 
       return;
